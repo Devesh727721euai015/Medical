ad7c5ef1911a5b05cffac2ca66b5a9c3d653a2bd
Devesh727721euai015 <126043610+Devesh727721euai015@users.noreply.github.com>
2023-03-08 15:07:42 +0530

    Add files via upload

---
+++ Reactpro/src/main/java/com/example/demo/ReactproApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactproApplication.class, args);
+	}
+
+}
